6274853914327bcd38ef23695f472907db88aae4
WongKi <462929012@qq.com>
2019-07-16 20:49:47 +0800

    多余代码注释掉

--- app/src/main/java/com/wongki/demo/vm/MusicViewModel.kt
+++ app/src/main/java/com/wongki/demo/vm/MusicViewModel.kt
@@ -32,27 +32,27 @@ class MusicViewModel : AbsLiveDataViewModel() {
         /**
          * 如果需要保存数据则，需要请求成功后的操作
          */
-        // 1. 启动远端仓库
-        launchRemoteResp(MusicServiceCore) {
-            // 2. 远端仓库的api请求
-            searchMusic(name = name)
-
-            //3. 提交远端仓库的api请求
-        }.commitForArrayList()
-
-        // 4. 异步请求远端仓库的成功的回调
-        { result ->
-
-            if (!result.isNullOrEmpty()) {
-                // 5. 启动本地SP仓库
-                launchLocalSpResp {
-                    // 6. 保存第一条数据到SP
-                    val firstMusic = result.first()
-                    this.firstMusicAuthor = firstMusic.author
-                    this.firstMusicTitle = firstMusic.title
-                }
-            }
-
-        }
+//        // 1. 启动远端仓库
+//        launchRemoteResp(MusicServiceCore) {
+//            // 2. 远端仓库的api请求
+//            searchMusic(name = name)
+//
+//            //3. 提交远端仓库的api请求
+//        }.commitForArrayList()
+//
+//        // 4. 异步请求远端仓库的成功的回调
+//        { result ->
+//
+//            if (!result.isNullOrEmpty()) {
+//                // 5. 启动本地SP仓库
+//                launchLocalSpResp {
+//                    // 6. 保存第一条数据到SP
+//                    val firstMusic = result.first()
+//                    this.firstMusicAuthor = firstMusic.author
+//                    this.firstMusicTitle = firstMusic.title
+//                }
+//            }
+//
+//        }
     }
 }
